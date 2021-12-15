837850185158822eb197d7a3fae73a02f47f8df3
Monique Silva <moniquesntsilva@gmail.com>
2021-12-14 22:33:25 -0300

    feat: add name field

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -11,10 +11,6 @@
   </div>
 </template>
 
-<script>
-export default {}
-</script>
-
 <style scoped>
 div {
   width: 87%;
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -5,22 +5,27 @@
       alt="Background Pattern"
       class="card-image"
     />
-    <div class="testing">
-      <article class="card-information">
-        <div class="card-subsections">
-          <h2>80K</h2>
-          <p>Followers</p>
-        </div>
-        <div class="card-subsections">
-          <h2>803K</h2>
-          <p>Likes</p>
-        </div>
-        <div class="card-subsections">
-          <h2>1.4K</h2>
-          <p>Photos</p>
-        </div>
-      </article>
-    </div>
+    <article class="card-name-area">
+      <div class="card-title">
+        <h1>Victor Crest</h1>
+        <span>26</span>
+      </div>
+      <p class="card-city">London</p>
+    </article>
+    <article class="card-information">
+      <div class="card-subsections">
+        <h2>80K</h2>
+        <p>Followers</p>
+      </div>
+      <div class="card-subsections">
+        <h2>803K</h2>
+        <p>Likes</p>
+      </div>
+      <div class="card-subsections">
+        <h2>1.4K</h2>
+        <p>Photos</p>
+      </div>
+    </article>
   </section>
 </template>
 
@@ -44,6 +49,36 @@
 .card-image {
   border-radius: 15px 15px 0 0;
   width: 100%;
+}
+
+.card-name-area {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  font-family: var(--kumbh);
+}
+
+.card-title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+}
+
+.card-title h1 {
+  color: var(--dark-blue);
+  font-size: var(--size);
+}
+
+.card-title span {
+  color: var(--grayish-blue);
+}
+
+.card-city {
+  color: var(--grayish-blue);
+  font-size: 1rem;
 }
 
 .card-information {
@@ -77,7 +112,7 @@
 }
 
 .card-subsections p {
-  color: var(--dark-gray);
+  color: var(--grayish-blue);
   font-family: var(--kumbh);
   font-size: 0.625rem;
   letter-spacing: 1px;
